feebf8e845e10c3678504f37cbbee18a2ee9b36c
Rofifah Nurul Annisa <rofifahnurul00@gmail.com>
2020-06-10 23:13:27 +0800

    Project

---
+++ Project.js
@@ -0,0 +1,4 @@
+var body = document.getElementsByTagName("body")[0];
+//body.style.backgroundImage = "url(bgfix.jpg)";
+//var aside0 = document.getElementsByTagName("aside")[0];
+//aside0.style.float = "right";
