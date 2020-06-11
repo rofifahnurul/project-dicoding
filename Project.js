95ccd57de72ac3e6d66fc8a5c817c87de5547f53
Rofifah Nurul Annisa <rofifahnurul00@gmail.com>
2020-06-11 14:00:03 +0800

    Js

--- Project.js
+++ Project.js
@@ -1,4 +1,15 @@
-var body = document.getElementsByTagName("body")[0];
-//body.style.backgroundImage = "url(bgfix.jpg)";
-//var aside0 = document.getElementsByTagName("aside")[0];
-//aside0.style.float = "right";
+var foto1 = document.getElementById("foto1");
+var foto2 = document.getElementById("foto2");
+foto1.addEventListener("mouseover", function() {
+    foto1.setAttribute("src", "assets/foto3.jpg");
+})
+
+foto2.addEventListener("mouseover", function() {
+    foto2.setAttribute("src", "assets/foto4.jpg");
+})
+foto1.addEventListener("mouseout", function() {
+    foto1.setAttribute("src", "assets/foto1.jpg");
+})
+foto2.addEventListener("mouseout", function() {
+    foto2.setAttribute("src", "assets/foto2.jpg");
+})
